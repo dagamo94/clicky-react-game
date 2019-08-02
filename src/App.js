e6c01dfa707ed43a21cefb162cb6d47d6bfaaf2f
main desktop <da.ga.mo.94@gmail.com>
2019-08-02 01:53:24 -0600

    scoring works. Need to add shuffle cards functionality and styling

--- src/App.js
+++ src/App.js
@@ -14,14 +14,36 @@ class App extends Component {
     highestScore: 0
   }
 
+  shuffleCards = () => {
+
+  }
+
   selectedCard = id => {
     console.log(id);
+    const selectedArr = this.state.selectedCards;
+    // this.shuffleCards();
+      console.log("click");
+      if (selectedArr.includes(id)) {
+        this.setState({
+          selectedCards: [],
+          highestScore: this.state.highestScore > this.state.score ? this.state.highestScore : this.state.score,
+          score: 0,
+        })
+      } else {
+        this.state.selectedCards.push(id);
+        this.setState({
+          score: this.state.selectedCards.length,
+        })
+      }
   }
 
   render() {
     return (
       <>
-        <NavBar>Clicky Memory Game</NavBar>
+        <NavBar>
+         <div type="clicky"> Clicky Memory Game </div>
+        <h3 className="float-right mt-60 score">Score: {this.state.score} | Highest Score: {this.state.highestScore}</h3>
+        </NavBar>
         <Title>
           <h1 className="display-4">Clicky Memory Game!</h1>
           <p className="lead">Click on an image to earn points, but don't click on a any more than once!</p>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,25 +2,20 @@ import React from "react";
 
 const NavBar = props => {
     return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        {props.children}
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+            <a className="navbar-brand">{props.children}</a>
             <ul className="navbar-nav">
                 <li className="nav-item active">
-                    {props.children}
+                    {/* {props.children} */}
                 </li>
                 <li className="nav-item">
-                    Features
+                    {/* Features */}
                 </li>
                 <li className="nav-item">
-                    Pricing
+                    {/* Pricing */}
                 </li>
             </ul>
-        </div>
-    </nav>
+        </nav>
     )
 }
 
